efac551b783d64067cfae74eef1251feebddc804
Jim Kang <jimkang@gmail.com>
2015-03-23 12:30:25 -0400

    Refactored tests.

--- tests/levelnamer-tests.js
+++ tests/levelnamer-tests.js
@@ -1,10 +1,35 @@
 var test = require('tape');
 var levelnamer = require('../index');
 
-test('Get a name level for a word.', function basicTest(t) {
-  t.plan(13);
+function runNamingTest(testTitle, nameOpts, expectedResults) {
+  test(testTitle, function testIt(t) {
+    t.plan(nameOpts.totalLevels + 1);
 
-  var expectedResults = [
+    levelnamer.getNamedLevels(nameOpts, checkResults);
+
+    function checkResults(error, levelNames) {
+      if (error) {
+        console.log(error);
+      }
+      t.ok(!error, 'Completes without an error.');
+      levelNames.forEach(checkLevel);
+    }
+
+    function checkLevel(levelName, i) {
+      t.equal(
+        levelName, expectedResults[i], 'The '+ (i+1) + 'th level is correct.'
+      );
+    }
+  });
+}
+
+runNamingTest(
+  'Get a name level for a word.',
+  {
+    word: 'yob',
+    totalLevels: 12
+  },
+  [
     'Plug-ugly',
     'Muscleman',
     'Skinhead',
@@ -17,26 +42,5 @@ test('Get a name level for a word.', function basicTest(t) {
     'Yob',
     'Prime Yob',
     'Master Yob'
-  ];
-
-  var opts = {
-    word: 'yob',
-    totalLevels: 12
-    // wordnok: 
-    // random: 
-  };
-
-  levelnamer.getNamedLevels(opts, checkResults);
-
-  function checkResults(error, levelNames) {
-    if (error) {
-      console.log(error);
-    }
-    t.ok(!error, 'Completes without an error.');
-    levelNames.forEach(function checkLevel(levelName, i) {
-      t.equal(
-        levelName, expectedResults[i], 'The '+ (i+1) + 'th level is correct.'
-      );
-    });
-  }
-});
+  ]
+);
