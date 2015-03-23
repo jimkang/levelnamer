a83890d37774529a3f3067fa429560b0347adad6
Jim Kang <jimkang@gmail.com>
2015-03-23 15:27:27 -0400

    Updated run-levelnamer to work with param change.

--- tools/run-levelnamer.js
+++ tools/run-levelnamer.js
@@ -1,5 +1,7 @@
 var levelnamer = require('../index');
 var _ = require('lodash');
+var seedrandom = require('seedrandom');
+var createProbable = require('probable').createProbable;
 
 if (process.argv.length < 3) {
   console.log('Usage: node run-levelnamer.js <base title>');
@@ -8,9 +10,14 @@ if (process.argv.length < 3) {
 
 var baseTitle = process.argv[2];
 
+var probable = createProbable({
+  random: seedrandom(baseTitle)
+});
+
 levelnamer.getNamedLevels(
   {
-    word: process.argv[2]
+    word: process.argv[2],
+    totalLevels: 12 + probable.rollDie(12)
   },
   function displayResults(error, results) {
     if (error) {
